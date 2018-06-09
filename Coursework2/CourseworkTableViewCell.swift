e2b06399ca25f092da35445506c18715c5a1affd
Jeshreen Balraj <balraj@Jeshreens-MacBook-Pro.local>
2018-06-09 20:02:02 +0530

    addition of the code

---
+++ Coursework2/CourseworkTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  CourseworkTableViewCell.swift
+//  Coursework2
+//
+//  Created by Jeshreen Balraj on 5/30/18.
+//  Copyright © 2018 Jeshreen Balraj. All rights reserved.
+//
+
+import UIKit
+
+class CourseworkTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
